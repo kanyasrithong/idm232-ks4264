4e39efdc1226635de1ccaaf19f3322f083d7cd50
kanyasrithong <ks4264@drexel.edu>
2025-11-19 22:42:25 -0500

    Keep database open

--- db.php
+++ db.php
@@ -17,9 +17,6 @@
   $connection = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
   if ($connection->connect_error)
-      die("Connection failed: " . $connection->connect_error);
+      die("Connection failed: {$connection->connect_error}");
 
-  echo "Connected successfully";
 
-  $connection->close();
- ?>
