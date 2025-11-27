fe83a4fb47c92399395bbd3a2dc383075ba55c99
kanyasrithong <ks4264@drexel.edu>
2025-11-27 16:48:05 -0500

    Dynamic recipe info

--- recipe.php
+++ recipe.php
@@ -1,71 +1,80 @@
+<?php
+  require_once 'db.php';
+
+  if(isset($_GET['id'])) {
+    $search_query = $_GET['id'];
+    $stmt = $connection->prepare('SELECT heading, subheading, description, ingredients, steps, img_folder, hero_lg, hero_sm, step1_img_lg, step1_img_sm, step2_img_lg, step2_img_sm, step3_img_lg, step3_img_sm, step4_img_lg, step4_img_sm, step5_img_lg, step5_img_sm, step6_img_lg, step6_img_sm  FROM RECIPES WHERE id = ?');
+    $stmt->bind_param("i", $search_query);
+    $stmt->execute();
+    $stmt->bind_result($heading, $subheading, $description, $ingredients, $steps, $img_folder, $hero_lg, $hero_sm, $step1_img_lg, $step1_img_sm, $step2_img_lg, $step2_img_sm, $step3_img_lg, $step3_img_sm, $step4_img_lg, $step4_img_sm, $step5_img_lg, $step5_img_sm, $step6_img_lg, $step6_img_sm);
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Recipe</title>
-  <link rel="stylesheet" href="styles/styles.css">
-  <link rel="stylesheet" href="styles/queries.css">
-  <link rel="stylesheet" href="styles/recipe.css">
+  <link rel="stylesheet" href="/styles/styles.css">
+  <link rel="stylesheet" href="/styles/queries.css">
+  <link rel="stylesheet" href="/styles/recipe.css">
 </head>
 <body>
   <header>
     <a id="home-icon" href="index.php">Cookbook</a>
     <div id="search-bar">
-      <img id="search-icon" src="assets/icons/search-icon.svg" alt="Search icon">
+      <img id="search-icon" src="/assets/icons/search-icon.svg" alt="Search icon">
       <input id="search-input" placeholder="Find a new recipe..." type="text">
     </div>
   </header>
-  <div id="hero">
-    <div id="details">
-      <div id="recipe-title">
-        <h1>Roasted Turkey Breast & Farro-Endive Salad</h1>
-        <h2>with Miso Mayonnaise & Roasted Sweet Potatoes</h2>
+  <?php
+    while($stmt->fetch()) : ?>
+      <div id="hero">
+        <div id="details">
+          <div id="recipe-title">
+            <h1><?php echo $heading ?></h1>
+            <h2><?php echo $subheading ?></h2>
+          </div>
+          <p><?php echo $description ?></p>
+        </div>
+        <picture>
+          <source media="(min-width:980px)" srcset="<?php echo "/assets/images/{$img_folder}/$hero_lg" ?>">
+          <img src="<?php echo "/assets/images/{$img_folder}/$hero_sm" ?>" alt="Chicken and rice">
+        </picture>
       </div>
-    <p>
-      This quick-cooking recipe captures all the deliciously creamy, rich flavor of the classic Italian-American sauce—with the welcome addition of quick-braised broccoli. A dusting of pecorino cheese completes each bowl with a touch of sharpness.
-    </p>
-    </div>
-    <img src="recipe-thumbnail-test.jpg" alt="Chicken and rice">
-  </div>
-  <div id="recipe-content">
-    <aside>
-      <h2>Ingredients</h2>
-      <ul>
-        <li>2 Boneless, Skinless Chicken Breasts</li>
-        <li>2 Carrots</li>
-        <li>1 Red Onion</li>
-        <li>2 cloves Garlic</li>
-        <li>1/2 cup French Green Lentils</li>
-        <li>1 bunch Collard Greens</li>
-        <li>2 Tbsps Tomato Paste</li>
-        <li>1/4 tsp Crushed Red Pepper Flakes</li>
-        <li>1 Tbsp Capers</li>
-        <li>1 Tbsp Apple Cider Vinegar</li>
-        <li>2 Tbsps Crumbled Goat Cheese</li>
-        <li>1 Tbsp Tuscan Spice Blend (Ground Fennel Seeds, Whole Fennel Seeds, Ground Rosemary, & Ground Sage)</li>
-      </ul>
-      <img src="recipe-ingredients-test.png" alt="Ingredients">
-    </aside>
-    <main>
-      <div class="recipe-step">
-        <h1>Step 1</h1>
-        <p>
-          Remove the honey from the refrigerator to bring to room temperature. Place
-          an oven rack in the center of the oven; preheat to 450°F. Heat a medium pot
-          of salted water to boiling on high. Line a sheet pan with foil. Remove and
-          discard the netting from the turkey; season with salt and pepper on both
-          sides. In a large pan, heat 2 teaspoons of olive oil on medium-high until hot.
-          Add the seasoned turkey, skin side down. Cook 4 to 6 minutes on the first
-          side, or until lightly browned. Flip and cook 3 to 5 minutes, or until lightly
-          browned. Leaving any browned bits (or fond) in the pan on the stove, transfer
-          to the sheet pan, skin side up. Roast 15 to 17 minutes, or until cooked
-          through. (An instant-read thermometer should register 165ºF.) Transfer to a
-          cutting board and let rest for at least 5 minutes.
-        </p>
-        <img src="recipe-thumbnail-test.jpg" alt="Chicken and rice">
+      <div id="recipe-content">
+        <aside>
+          <h2>Ingredients</h2>
+          <ul>
+            <li>2 Boneless, Skinless Chicken Breasts</li>
+            <li>2 Carrots</li>
+            <li>1 Red Onion</li>
+            <li>2 cloves Garlic</li>
+            <li>1/2 cup French Green Lentils</li>
+            <li>1 bunch Collard Greens</li>
+            <li>2 Tbsps Tomato Paste</li>
+            <li>1/4 tsp Crushed Red Pepper Flakes</li>
+            <li>1 Tbsp Capers</li>
+            <li>1 Tbsp Apple Cider Vinegar</li>
+            <li>2 Tbsps Crumbled Goat Cheese</li>
+            <li>1 Tbsp Tuscan Spice Blend (Ground Fennel Seeds, Whole Fennel Seeds, Ground Rosemary, & Ground Sage)</li>
+          </ul>
+          <img src="<?php echo "/assets/images/{$img_folder}/ingredients.png" ?>" alt="Ingredients">
+        </aside>
+        <main>
+          <?php
+            $steps_array = explode("*", $steps);
+            foreach ($steps_array as $step => $value) : ?>
+            <?php $step++ ?>
+              <div class="recipe-step">
+                <h1>Step <?php echo $step ?></h1>
+                <p><?php echo $value ?></p>
+                <img src="<?php echo "/assets/images/{$img_folder}/step{$step}-lg.jpg" ?>" alt="Chicken and rice">
+              </div>
+          <?php endforeach ?>
+        </main>
       </div>
-    </main>
-  </div>
+  <?php endwhile ?>
 </body>
 </html>
