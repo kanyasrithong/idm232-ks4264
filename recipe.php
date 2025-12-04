ddb4c8cfa2a1ace033c96612636b6307b2371966
kanyasrithong <ks4264@drexel.edu>
2025-12-04 14:11:13 -0500

    Dynamic ingredients

--- recipe.php
+++ recipe.php
@@ -3,7 +3,7 @@
 
   if(isset($_GET['id'])) {
     $search_query = $_GET['id'];
-    $stmt = $connection->prepare('SELECT * FROM recipes WHERE id = ?');
+    $stmt = $connection->prepare('SELECT id, heading, subheading, description, ingredients, steps, img_folder, hero_lg, hero_sm FROM recipes WHERE id = ?');
     $stmt->bind_param("i", $search_query);
     $stmt->execute();
     $stmt->bind_result($id, $heading, $subheading, $description, $ingredients, $steps, $img_folder, $hero_lg, $hero_sm);
@@ -48,18 +48,11 @@
         <aside>
           <h2>Ingredients</h2>
           <ul>
-            <li>2 Boneless, Skinless Chicken Breasts</li>
-            <li>2 Carrots</li>
-            <li>1 Red Onion</li>
-            <li>2 cloves Garlic</li>
-            <li>1/2 cup French Green Lentils</li>
-            <li>1 bunch Collard Greens</li>
-            <li>2 Tbsps Tomato Paste</li>
-            <li>1/4 tsp Crushed Red Pepper Flakes</li>
-            <li>1 Tbsp Capers</li>
-            <li>1 Tbsp Apple Cider Vinegar</li>
-            <li>2 Tbsps Crumbled Goat Cheese</li>
-            <li>1 Tbsp Tuscan Spice Blend (Ground Fennel Seeds, Whole Fennel Seeds, Ground Rosemary, & Ground Sage)</li>
+            <?php
+              $ingredients_array = explode("*", $ingredients);
+              foreach ($ingredients_array as $num => $value) : ?>
+              <li><?php echo $value?></li>
+            <?php endforeach ?>
           </ul>
           <img src="<?php echo "assets/images/{$img_folder}/ingredients.png" ?>" alt="Ingredients">
         </aside>
