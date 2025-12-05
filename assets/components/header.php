6599f4f950e380e3d08e2f5c300b12325577a4f8
kanyasrithong <ks4264@drexel.edu>
2025-12-04 21:37:22 -0500

    Search redirect

--- assets/components/header.php
+++ assets/components/header.php
@@ -2,7 +2,7 @@
   <a id="home-icon" href="index.php">Cookbook</a>
   <div id="search-bar">
     <img id="search-icon" src="assets/icons/search-icon.svg" alt="Search icon">
-    <form method="post">
+    <form action="/" method="post">
       <input id="search-input" name="search" placeholder="Find a new recipe..." type="text">
       <input type="submit" hidden />
     </form>
