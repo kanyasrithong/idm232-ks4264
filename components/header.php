ea2c70026a3ef59ca058d609c5a2acfa118f9212
kanyasrithong <ks4264@drexel.edu>
2025-12-04 21:42:08 -0500

    Fix redirect

--- components/header.php
+++ components/header.php
@@ -2,7 +2,7 @@
   <a id="home-icon" href="index.php">Cookbook</a>
   <div id="search-bar">
     <img id="search-icon" src="assets/icons/search-icon.svg" alt="Search icon">
-    <form action="/" method="post">
+    <form action="index.php" method="post">
       <input id="search-input" name="search" placeholder="Find a new recipe..." type="text">
       <input type="submit" hidden />
     </form>
